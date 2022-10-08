dd14f7cd2a87f517ad53ce6d8e6e902254ae75ae
Ruth <88747761+AwareRuuh@users.noreply.github.com>
2022-10-08 17:45:54 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+class Main {
+  public static void main(String[] args) {
+    String[] cores = {"Azul","Verde","Roxo"};
+    String lista = Arrays.toString(cores);
+      System.out.println("As cores são: " +lista);
+    System.out.println("As cores são: " +lista);
+    System.out.println("As cores são: " +lista);
+    System.out.println("As cores são: " +lista);
+    System.out.println("As cores são: " +lista);
+    System.out.println("As cores são: " +lista);
+    System.out.println("As cores são: " +lista);
+    System.out.println("As cores são: " +lista);
+    System.out.println("As cores são: " +lista);
+    System.out.println("As cores são: " +lista);
+    System.out.println("As cores são: " +lista);
+    System.out.println("As cores são: " +lista);
+    System.out.println("As cores são: " +lista);
+    System.out.println("As cores são: " +lista);
+    System.out.println("As cores são: " +lista);
+    System.out.println("As cores são: " +lista);
+    System.out.println("As cores são: " +lista);
+    System.out.println("As cores são: " +lista);
+    System.out.println("As cores são: " +lista);
+    System.out.println("As cores são: " +lista);
+    System.out.println("As cores são: " +lista);
+    System.out.println("As cores são: " +lista);
+    System.out.println("As cores são: " +lista);
+    System.out.println("As cores são: " +lista);
+    System.out.println("As cores são: " +lista);
+    System.out.println("As cores são: " +lista);
+    System.out.println("As cores são: " +lista);
+    System.out.println("As cores são: " +lista);
+    System.out.println("As cores são: " +lista);
+    System.out.println("As cores são: " +lista);
+  }
+      }
